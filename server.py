50725c73670e04afae93c0bf5b36c0d2e87fe2a8
Sanjeev Rijal <hopsanjeev@gmail.com>
2024-04-22 08:37:46 +1000

    update

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import request, jsonify, redirect
+from model import User, Bill,Split
 from config import app
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
